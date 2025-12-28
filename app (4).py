f0daff0a260827136b630197af89716adf4d0aad
suryanshaxis-ctrl <suryanshaxis@gmail.com>
2025-12-28 12:11:14 +0530

    Add files via upload

---
+++ app (4).py
@@ -0,0 +1,10 @@
+import streamlit as st
+import time
+import pandas as pd
+import numpy as np 
+from sklearn.preprocessing import StandardScaler
+import pickle
+from sklearn.datasets import fetch_california_housing
+
+st.title('🏡 HOUSE PRICE PREDICTION USING ML')
+st.image('https://media1.giphy.com/media/v1.Y2lkPTZjMDliOTUyaXhmZjBqdzcxZ2NmY3R5djV3YTI1M2pobDVrNGlxMmhlOHV3ZmpmaCZlcD12MV9naWZzX3NlYXJjaCZjdD1n/l0Iy7ez9MiA0ChmbC/200w.gif')
